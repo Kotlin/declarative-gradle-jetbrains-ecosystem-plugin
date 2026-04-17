ffb77c0e751790cac97edbba71cdd2c1c2cdda3d
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-04-17 16:10:16 +0200

    Introduce a concept of "compilation unit" build feature
    Compilation unit is a base build feature describing common requirements to produce some compiled output using a set of sources.
    
    Compilation unit is not expected to be used directly, and more specialized view should be composed for the specific type of compilation.

---
+++ common/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/common/buildtypes/JvmCompilationUnit.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.kotlin.gradle.declarative.common.buildtypes
+
+import org.gradle.api.Named
+import org.gradle.api.PolymorphicDomainObjectContainer
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.Directory
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Provider
+import org.gradle.jvm.toolchain.JavaToolchainSpec
+
+public interface JvmEcosystem {
+    public val implementationConfiguration: Configuration
+    public val compileOnlyConfiguration: Configuration
+    public val compilationClasspath: FileCollection
+    public val jdkToolchain: JavaToolchainSpec
+}
+
+public sealed interface JvmCompilationType : Named {
+    public val compilationName: String
+
+    override fun getName(): String = compilationName
+}
+public interface KotlinJvmCompilationType : JvmCompilationType {
+    // Missing Kotlin compiler args
+    public val kotlinCompilerClasspath: ConfigurableFileCollection
+}
+
+public interface JavaJvmCompilationType : JvmCompilationType {
+    // Missing Java compiler args
+    public val javacPath: RegularFileProperty
+}
+
+public interface JvmCompilationUnit : CompilationUnit {
+    public val resourcesOutput: Provider<Directory>
+    public val jvmEcosystem: JvmEcosystem
+    public val jvmCompilations: PolymorphicDomainObjectContainer<JvmCompilationType>
+}
--- project-types/jvm-application/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/jvmapplication/JvmApplicationBuildModel.kt
+++ project-types/jvm-application/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/jvmapplication/JvmApplicationBuildModel.kt
@@ -2,18 +2,34 @@ package org.jetbrains.kotlin.gradle.declarative.projecttypes.jvmapplication
 
 import org.gradle.api.Named
 import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.PolymorphicDomainObjectContainer
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.features.binding.BuildModel
+import org.gradle.jvm.toolchain.JavaToolchainSpec
+import org.jetbrains.kotlin.gradle.declarative.common.buildtypes.JavaJvmCompilationType
+import org.jetbrains.kotlin.gradle.declarative.common.buildtypes.JvmCompilationType
+import org.jetbrains.kotlin.gradle.declarative.common.buildtypes.JvmCompilationUnit
+import org.jetbrains.kotlin.gradle.declarative.common.buildtypes.JvmEcosystem
+import org.jetbrains.kotlin.gradle.declarative.common.buildtypes.KotlinJvmCompilationType
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import javax.inject.Inject
 
 @Suppress("UnstableApiUsage")
 public interface JvmApplicationBuildModel : BuildModel {
+    public val compilationUnits: NamedDomainObjectContainer<JvmCompilationUnit>
     public val applications: NamedDomainObjectContainer<JvmApplication>
 }
 
@@ -24,7 +40,7 @@ public interface JvmApplication : Named {
     public val jvmArgs: ListProperty<String>
 
     public val compiledClasses: ConfigurableFileCollection
-    public val dependencies: ConfigurableFileCollection
+    public val runtimeDependencies: ConfigurableFileCollection
 
     public val runTask: TaskProvider<JavaExec>
     public val executionDirectory: DirectoryProperty
@@ -37,10 +53,81 @@ internal interface InternalJvmApplication : JvmApplication {
 }
 
 internal abstract class DefaultJvmApplicationBuildModel @Inject constructor(
-    objectFactory: ObjectFactory,
+    private val objectFactory: ObjectFactory,
 ) : JvmApplicationBuildModel {
+    override val compilationUnits: NamedDomainObjectContainer<JvmCompilationUnit> =
+        objectFactory.domainObjectContainer(JvmCompilationUnit::class.java) { name ->
+            objectFactory.newInstance(DefaultJvmCompilationUnit::class.java, name)
+        }
+
     @Suppress("UNCHECKED_CAST")
     override val applications: NamedDomainObjectContainer<JvmApplication> = objectFactory
         .domainObjectContainer(InternalJvmApplication::class.java) as NamedDomainObjectContainer<JvmApplication>
+
+    abstract class DefaultJvmCompilationUnit @Inject constructor(
+        private val entityName: String,
+        private val objectFactory: ObjectFactory,
+        providerFactory: ProviderFactory,
+    ) : JvmCompilationUnit {
+        override fun getName(): String = entityName
+
+        lateinit var kotlinCompilation: KotlinCompilation<*>
+
+        override val sources: SourceDirectorySet
+            get() = kotlinCompilation.defaultSourceSet.kotlin
+
+        override val destinationDirectory: DirectoryProperty
+            get() = kotlinCompilation.defaultSourceSet.kotlin.destinationDirectory
+
+        override val outputs: FileCollection
+            get() = kotlinCompilation.output.classesDirs
+
+        override val resourcesOutput: Provider<Directory>
+            get() = objectFactory.directoryProperty().fileValue(kotlinCompilation.output.resourcesDir)
+
+        override val jvmEcosystem: JvmEcosystem =
+            objectFactory.newInstance(DefaultJvmEcosystem::class.java, this)
+
+        override val jvmCompilations: PolymorphicDomainObjectContainer<JvmCompilationType> =
+            objectFactory
+                .polymorphicDomainObjectContainer(JvmCompilationType::class.java)
+                .apply {
+                    registerFactory(KotlinJvmCompilationType::class.java) { name ->
+                        objectFactory.newInstance(DefaultKotlinJvmCompilationType::class.java, name)
+                    }
+                    registerFactory(JavaJvmCompilationType::class.java) { name ->
+                        objectFactory.newInstance(DefaultJavaJvmCompilationType::class.java, name)
+                    }
+                }
+
+        abstract class DefaultJvmEcosystem @Inject constructor(
+            private val compilationUnit: DefaultJvmCompilationUnit
+        ) : JvmEcosystem {
+            private val kotlinCompilation
+                get() = compilationUnit.kotlinCompilation
+
+            override val implementationConfiguration: Configuration
+                get() = kotlinCompilation.project.configurations
+                    .getByName(kotlinCompilation.defaultSourceSet.implementationConfigurationName)
+
+            override val compileOnlyConfiguration: Configuration
+                get() = kotlinCompilation.project.configurations
+                    .getByName(kotlinCompilation.defaultSourceSet.compileOnlyConfigurationName)
+
+            override val compilationClasspath: FileCollection
+                get() = kotlinCompilation.compileDependencyFiles
+
+            override val jdkToolchain: JavaToolchainSpec
+                get() = kotlinCompilation.project.extensions.getByType(JavaPluginExtension::class.java).toolchain
+        }
+
+        abstract class DefaultKotlinJvmCompilationType @Inject constructor(
+            override val compilationName: String
+        ) : KotlinJvmCompilationType
+
+        abstract class DefaultJavaJvmCompilationType @Inject constructor(
+            override val compilationName: String
+        ) : JavaJvmCompilationType
+    }
 }
 
