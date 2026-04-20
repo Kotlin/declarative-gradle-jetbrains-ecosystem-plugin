ab0f5502b149a040c953383e1d632b599ea13219
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-04-20 16:17:09 +0200

    Add 'java {}' definition
    It allows now defining Java compilation arguments as free form list.

--- project-types/jvm-application/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/jvmapplication/JvmApplicationProjectType.kt
+++ project-types/jvm-application/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/jvmapplication/JvmApplicationProjectType.kt
@@ -2,12 +2,16 @@ package org.jetbrains.kotlin.gradle.declarative.projecttypes.jvmapplication
 
 import org.gradle.api.tasks.Nested
 import org.gradle.features.binding.Definition
+import org.jetbrains.kotlin.gradle.declarative.common.definitions.JavaJvmCompilationExtension
 import org.jetbrains.kotlin.gradle.declarative.common.definitions.JvmEcosystemDefinition
 
 @Suppress("UnstableApiUsage")
 public interface JvmApplicationProjectType : Definition<JvmApplicationBuildModel>,
     ApplicationDefinition,
     JvmEcosystemDefinition {
+
+    @get:Nested
+    public val java: JavaJvmCompilationExtension
 
     @get:Nested
     public val dependencies: JvmApplicationDependenciesExtension
