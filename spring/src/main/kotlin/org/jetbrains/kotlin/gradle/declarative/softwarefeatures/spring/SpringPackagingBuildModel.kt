401edd8a8ac3fe9b011bb01ef40afa88f837c43b
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-04-28 14:40:05 +0200

    Add Spring packaging software feature

---
+++ spring/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/softwarefeatures/spring/SpringPackagingBuildModel.kt
@@ -0,0 +1,7 @@
+package org.jetbrains.kotlin.gradle.declarative.softwarefeatures.spring
+
+import org.gradle.features.binding.BuildModel
+
+@Suppress("UnstableApiUsage")
+public interface SpringPackagingBuildModel : BuildModel {
+}
