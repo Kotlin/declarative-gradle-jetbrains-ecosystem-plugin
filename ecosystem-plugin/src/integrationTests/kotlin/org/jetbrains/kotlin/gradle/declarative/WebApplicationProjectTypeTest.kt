acfdaa7cd27a30f69d4f56627da9648de8905c90
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-04-29 11:24:01 +0200

    Configure JS/browser type of project

--- ecosystem-plugin/src/integrationTests/kotlin/org/jetbrains/kotlin/gradle/declarative/WebApplicationProjectTypeTest.kt
+++ ecosystem-plugin/src/integrationTests/kotlin/org/jetbrains/kotlin/gradle/declarative/WebApplicationProjectTypeTest.kt
@@ -1,15 +1,22 @@
 package org.jetbrains.kotlin.gradle.declarative
 
+import org.gradle.api.project.IsolatedProject
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.declarative.testDsl.BaseTest
+import org.jetbrains.kotlin.gradle.declarative.testDsl.BuildOptions
 import org.jetbrains.kotlin.gradle.declarative.testDsl.GradleTest
+import org.jetbrains.kotlin.gradle.declarative.testDsl.assertTasksExecuted
 import org.jetbrains.kotlin.gradle.declarative.testDsl.build
 import org.jetbrains.kotlin.gradle.declarative.testDsl.project
+import org.jetbrains.kotlin.gradle.declarative.testDsl.source
 import org.junit.jupiter.api.DisplayName
 import kotlin.io.path.writeText
 
 @DisplayName("Web application project type")
 class WebApplicationProjectTypeTest : BaseTest() {
+
+    override val defaultBuildOptions: BuildOptions
+        get() = super.defaultBuildOptions.copy(isolatedProjects = BuildOptions.IsolatedProjectsMode.DISABLED)
 
     @DisplayName("smoke")
     @GradleTest
@@ -24,6 +31,35 @@ class WebApplicationProjectTypeTest : BaseTest() {
             )
 
             build("help")
+        }
+    }
+
+    @DisplayName("compile Browser code")
+    @GradleTest
+    fun testCompilerBrowserCode(gradleVersion: GradleVersion) {
+        project("base-ecosystem-project", gradleVersion) {
+            buildGradleDcl.writeText(
+                //language=declarative
+                """
+                |webApplication {
+                |}
+                """.trimMargin()
+            )
+
+            kotlinSourcesDir("jsMain").source("main.kt") {
+                //language=kotlin
+                """
+                |package org.example
+                |
+                |fun main() {
+                |    println("Hello, web application!")
+                |}
+                """.trimMargin()
+            }
+
+            build("jsBrowserDevelopmentExecutableDistribution", forwardBuildOutput = true) {
+                assertTasksExecuted(":jsBrowserDevelopmentExecutableDistribution")
+            }
         }
     }
 }
