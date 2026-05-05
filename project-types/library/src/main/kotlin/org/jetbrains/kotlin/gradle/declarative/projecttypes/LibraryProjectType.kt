800f7b9e0ede43b18483bb57d4fe6c9b05573bb2
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-05-05 18:03:40 +0200

    Add 'platforms' definition
    Allows selecting which platforms are available in the library.

--- project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryProjectType.kt
+++ project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryProjectType.kt
@@ -1,7 +1,17 @@
 package org.jetbrains.kotlin.gradle.declarative.projecttypes
 
+import org.gradle.api.provider.ListProperty
 import org.gradle.features.binding.Definition
 
 @Suppress("UnstableApiUsage")
 public interface LibraryProjectType : Definition<LibraryBuildModel> {
+    // Change to LibraryPlatforms type once https://github.com/gradle/gradle/issues/34114 is fixed
+    /**
+     * See available platforms at [LibraryPlatforms].
+     */
+    public val platforms: ListProperty<String>
+}
+
+public enum class LibraryPlatforms {
+    jvm, common, web, ios,
 }
