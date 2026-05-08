98b53373c00a95bd6d48c29705ceb95c13e20e91
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-05-08 20:43:30 +0200

    Move 'kotlin' into JvmEcosystemDefinition

--- project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryJvmEcosystemDefinition.kt
+++ project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryJvmEcosystemDefinition.kt
@@ -1,14 +1,8 @@
 package org.jetbrains.kotlin.gradle.declarative.projecttypes
 
-import org.gradle.api.tasks.Nested
 import org.gradle.features.binding.BuildModel
 import org.gradle.features.binding.Definition
 import org.jetbrains.kotlin.gradle.declarative.common.definitions.JvmEcosystemDefinition
-import org.jetbrains.kotlin.gradle.declarative.common.definitions.KotlinJvmCompilationExtension
 
 @Suppress("UnstableApiUsage")
-public interface LibraryJvmEcosystemDefinition : Definition<BuildModel.None>, JvmEcosystemDefinition {
-
-    @get:Nested
-    public val kotlin: KotlinJvmCompilationExtension
-}
+public interface LibraryJvmEcosystemDefinition : Definition<BuildModel.None>, JvmEcosystemDefinition
