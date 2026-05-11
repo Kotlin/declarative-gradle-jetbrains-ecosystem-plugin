028f4b83e21fa3936e7afbb8aec1eb5538a45da6
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-05-11 21:42:11 +0200

    Add testing platforms specific configuration in 'library' project type

--- project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryTestingExtension.kt
+++ project-types/library/src/main/kotlin/org/jetbrains/kotlin/gradle/declarative/projecttypes/LibraryTestingExtension.kt
@@ -1,10 +1,35 @@
 package org.jetbrains.kotlin.gradle.declarative.projecttypes
 
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Nested
+import org.gradle.features.binding.BuildModel
+import org.gradle.features.binding.Definition
 import org.jetbrains.kotlin.gradle.declarative.common.definitions.TestingExtension
 
 public interface LibraryTestingExtension : TestingExtension {
 
     @get:Nested
     public val dependencies: LibraryTestingDependenciesExtension
+
+    @get:Nested
+    public val jvmPlatform: LibraryTestingJvmEcosystemDefinition
+
+    @get:Nested
+    public val webPlatform: LibraryTestingWebEcosystemDefinition
+
+    @get:Nested
+    public val iosPlatform: LibraryTestingIosEcosystemDefinition
 }
+
+@Suppress("UnstableApiUsage")
+public interface LibraryTestingJvmEcosystemDefinition : Definition<BuildModel.None> {
+    public val useJunitPlatform: Property<Boolean>
+}
+
+@Suppress("UnstableApiUsage")
+public interface LibraryTestingWebEcosystemDefinition : Definition<BuildModel.None> {
+    public val skip: Property<Boolean>
+}
+
+@Suppress("UnstableApiUsage")
+public interface LibraryTestingIosEcosystemDefinition : Definition<BuildModel.None>
