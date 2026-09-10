4001fe19717accc2b18c2dd3c4bbed00b85882b9
Matej Tomášek <matej.tomasek@jetbrains.com>
2026-09-10 15:10:52 +0200

    Update Gradle to 9.7.1 release

--- ecosystem-plugin/src/integrationTests/kotlin/org/jetbrains/kotlin/gradle/declarative/testDsl/TestVersions.kt
+++ ecosystem-plugin/src/integrationTests/kotlin/org/jetbrains/kotlin/gradle/declarative/testDsl/TestVersions.kt
@@ -2,10 +2,10 @@ package org.jetbrains.kotlin.gradle.declarative.testDsl
 
 interface TestVersions {
     object Gradle {
-        const val G_9_6 = "9.6.1"
+        const val G_9_7 = "9.7.1"
 
-        const val MIN_SUPPORTED = G_9_6
-        const val MAX_SUPPORTED = G_9_6
+        const val MIN_SUPPORTED = G_9_7
+        const val MAX_SUPPORTED = G_9_7
     }
 
     object Kotlin {
